371040d6563c48f64cc538feb4198403667c09ae
kadun1 <kadunjj@gmail.com>
2022-03-05 01:55:51 +0900

    스프링기본편 1~3일차(순수 자바코드로 SOLID 구현하기)

---
+++ src/main/java/hello/core/SpringBasicInflearnApplication.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicInflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicInflearnApplication.class, args);
+    }
+
+}
